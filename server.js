d79e3e982da3c670f41dc27f6b96531bd679d328
Andy Lindsay <andydlindsay@gmail.com>
2017-03-10 05:13:12 +0000

    project complete

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '/templates'));
+// app.use(express.static(__dirname + '/templates'));
 
 app.get('*', function (req, res) {
-    res.render('index.html');
+    var responseObj = {
+        ipaddress: req.headers['x-forwarded-for'],
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
+    };
+    // console.log(req.headers);
+    // console.log(req.rawHeaders[7]);
+    res.send(responseObj);
 });
 
 app.listen(process.env.PORT, function () {
